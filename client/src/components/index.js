68a5c9198db464e0fb680b0ccde6cc2dda5928b9
Nathaniel D. Liebhart <nathaniel95975@gmail.com>
2026-01-28 15:43:30 -0600

    implemented the ui for the Home page

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Banner from "#components/home/Banner";
+import Hero from "#components/home/Hero";
+import Features from "#components/home/Features";
+import Testimonial from "#components/home/Testimonial";
+import Title from "#components/home/Title";
+import CallToAction from "#components/home/CallToAction";
+import Footer from "#components/home/Footer";
+
+export { Banner, Hero, Features, Testimonial, Title, CallToAction, Footer };
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import {
+  Banner,
+  Hero,
+  Features,
+  Testimonial,
+  CallToAction,
+  Footer,
+} from "#components";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Hero />
+      <Features />
+      <Testimonial />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
